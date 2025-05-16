9fa300643293f002318f2c2cceeecee50b25db6d
Ricardo Salazar <therlokx@gmail.com>
2025-05-16 22:48:29 +0200

    jrse--team2

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getParam, } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init()
+console.log(product)
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
